548b7c9349fa399756d3c4f7afc69af13f89d83e
eventifyorg <eventifyorg@gmail.com>
2021-08-08 07:27:02 +0000

    first update

---
+++ README.md
@@ -0,0 +1,3 @@
+# RestaurantSQLAPI
+
+
---
+++ db.js
@@ -0,0 +1,19 @@
+const sql = require('mssql');
+var config = {
+  user: 'sa',
+  password: '?churcher/',
+  server: 'SENSEI',
+  database: 'MyRestaurant',
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then((pool) => {
+    console.log('Connected to MSSQL');
+    return pool;
+  })
+  .catch((err) =>
+    console.log('Database connection failed ! Bad Config: ', err)
+  );
+
+module.exports = { sql, poolPromise };
